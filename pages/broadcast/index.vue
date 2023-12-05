b547b7b549dade1195e2da73dd4eeb3c804c0e81
Yuri Datsenko <breakeneck@gmail.com>
2023-12-05 18:07:35 +0200

    updated elplus for nuxt

--- pages/broadcast/index.vue
+++ pages/broadcast/index.vue
@@ -3,9 +3,7 @@ const broadcasts = await useApi('/broadcast');
 </script>
 
 <template>
-
   <div p-10>
-
     <el-table :data="broadcasts" style="width: 100%">
       <el-table-column prop="date" label="Date" width="180" />
       <el-table-column prop="user.name" label="Name" width="180" />
